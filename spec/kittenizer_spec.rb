43bb7a68f58172832f91b8bd2bf2d2a2c30ba1f6
ken1flan <ken1flan@gmail.com>
2015-11-25 19:40:43 +0900

    Add specs

--- spec/kittenizer_spec.rb
+++ spec/kittenizer_spec.rb
@@ -5,7 +5,37 @@ describe Kittenizer do
     expect(Kittenizer::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  WORD_PAIRS = [
+    ["こんばんは", "こんばんは"],
+    ["こんにちは。", "こんにちはにゃ。"],
+    ["あのとき食べたアレは本当においしかった。。。", "あのとき食べたアレは本当においしかったにゃ。。。"],
+    ["あれは一度食べておかないとー", "あれは一度食べておかにゃいとー"],
+    ["バナナ", "バニャニャ"],
+    ["おねがいします", "おねがいしみゃす"],
+    ["おはよう", "おはにゃう"],
+    ["オハヨウ", "オハニャウ"],
+  ]
+
+  describe "String#kittenize" do
+    WORD_PAIRS.each do |word_pair|
+      it "returns #{word_pair[1]}" do
+        expect(word_pair[0].kittenize).to eq word_pair[1]
+      end
+    end
+  end
+
+  describe "String#kittenize!" do
+    WORD_PAIRS.each do |word_pair|
+      context "when @str = '#{word_pair[0]}', then @str.kittenize!" do
+        before do
+          @str = word_pair[0].dup
+          @str.kittenize!
+        end
+
+        it "changes #{word_pair[1]}" do
+          expect(@str).to eq word_pair[1]
+        end
+      end
+    end
   end
 end
