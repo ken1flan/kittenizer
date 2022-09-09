825f11cc04d9a6b53e9bdb5bf0ee3b1891afc375
KUDO Ken-ichi <ken1flan@gmail.com>
2022-09-09 15:27:42 +0900

    Add rubocop (#7)
    * Add rubocop
    
    * rubocop -A
    
    * Add supported ruby version
    
    * Disable rubocop Style/Documentation
    
    * Add rubocop-rspec
    
    * rubocop -A
    
    * Fix :cop:
    
    * Refactor spec

--- lib/kittenizer/string.rb
+++ lib/kittenizer/string.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
 class String
   def kittenize
-    result = self.dup
+    result = dup
     DICTIONARY.each do |word|
       result.gsub!(word[0], word[1])
     end
@@ -9,20 +12,21 @@ class String
 
   def kittenize!
     DICTIONARY.each do |word|
-      self.gsub!(word[0], word[1])
+      gsub!(word[0], word[1])
     end
     self
   end
 
   DICTIONARY = [
     [/([^。])(。+)/, '\1にゃ\2'],
-    [/な/, "にゃ"],
-    [/ナ/, "ニャ"],
-    [/ま/, "みゃ"],
-    [/マ/, "ミャ"],
-    [/よう/, "にゃう"],
-    [/ヨウ/, "ニャウ"]
-  ]
+    [/な/, 'にゃ'],
+    [/ナ/, 'ニャ'],
+    [/ま/, 'みゃ'],
+    [/マ/, 'ミャ'],
+    [/よう/, 'にゃう'],
+    [/ヨウ/, 'ニャウ']
+  ].freeze
 
   private_constant :DICTIONARY
 end
+# rubocop:enable Style/Documentation
--- spec/kittenizer_spec.rb
+++ spec/kittenizer_spec.rb
@@ -1,47 +1,56 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
+WORD_PAIRS = [
+  %w[こんばんは こんばんは],
+  %w[こんにちは。 こんにちはにゃ。],
+  %w[あのとき食べたアレは本当においしかった。。。 あのとき食べたアレは本当においしかったにゃ。。。],
+  %w[あれは一度食べておかないとー あれは一度食べておかにゃいとー],
+  %w[バナナ バニャニャ],
+  %w[おねがいします おねがいしみゃす],
+  %w[おはよう おはにゃう],
+  %w[オハヨウ オハニャウ]
+].freeze
+
 describe Kittenizer do
-  it 'has a version number' do
-    expect(Kittenizer::VERSION).not_to be nil
+  describe 'version' do
+    subject { Kittenizer::VERSION }
+
+    it { is_expected.not_to be_nil }
   end
 
-  WORD_PAIRS = [
-    ["こんばんは", "こんばんは"],
-    ["こんにちは。", "こんにちはにゃ。"],
-    ["あのとき食べたアレは本当においしかった。。。", "あのとき食べたアレは本当においしかったにゃ。。。"],
-    ["あれは一度食べておかないとー", "あれは一度食べておかにゃいとー"],
-    ["バナナ", "バニャニャ"],
-    ["おねがいします", "おねがいしみゃす"],
-    ["おはよう", "おはにゃう"],
-    ["オハヨウ", "オハニャウ"],
-  ]
+  describe 'String#kittenize' do
+    subject { str.kittenize }
 
-  describe "String#kittenize" do
     WORD_PAIRS.each do |word_pair|
-      it "returns #{word_pair[1]}" do
-        expect(word_pair[0].kittenize).to eq word_pair[1]
+      context "when str = '#{word_pair[0]}'" do
+        let(:str) { word_pair[0].dup }
+
+        it { is_expected.to eq word_pair[1] }
       end
     end
   end
 
-  describe "String#kittenize!" do
-    WORD_PAIRS.each do |word_pair|
-      context "when @str = '#{word_pair[0]}', then @str.kittenize!" do
-        before do
-          @str = word_pair[0].dup
-          @str.kittenize!
-        end
+  describe 'String#kittenize!' do
+    WORD_PAIRS.reject { |item| item[0] == item[1] }.each do |word_pair|
+      context "when str = '#{word_pair[0]}'" do
+        let(:str) { word_pair[0].dup }
 
-        it "changes #{word_pair[1]}" do
-          expect(@str).to eq word_pair[1]
-        end
+        it { expect { str.kittenize! }.to change { str }.from(word_pair[0]).to(word_pair[1]) }
+      end
+    end
+
+    WORD_PAIRS.select { |item| item[0] == item[1] }.each do |word_pair|
+      context "when str = '#{word_pair[0]}'" do
+        let(:str) { word_pair[0].dup }
+
+        it { expect { str.kittenize! }.not_to(change { str }) }
       end
     end
   end
 
-  describe "String#DICTIONARY" do
-    it 'raises NameError' do
-      expect { String::DICTIONARY }.to raise_error(NameError)
-    end
+  describe 'String#DICTIONARY' do
+    it { expect { String::DICTIONARY }.to raise_error(NameError) }
   end
 end
