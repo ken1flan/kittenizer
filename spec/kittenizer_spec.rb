6da53bd4df148a9b30913a29aa5485086563315a
ken1flan <ken1flan@gmail.com>
2022-09-09 14:06:33 +0900

    Fix :cop:

--- spec/kittenizer_spec.rb
+++ spec/kittenizer_spec.rb
@@ -2,21 +2,21 @@
 
 require 'spec_helper'
 
+WORD_PAIRS = [
+  %w[こんばんは こんばんは],
+  %w[こんにちは。 こんにちはにゃ。],
+  %w[あのとき食べたアレは本当においしかった。。。 あのとき食べたアレは本当においしかったにゃ。。。],
+  %w[あれは一度食べておかないとー あれは一度食べておかにゃいとー],
+  %w[バナナ バニャニャ],
+  %w[おねがいします おねがいしみゃす],
+  %w[おはよう おはにゃう],
+  %w[オハヨウ オハニャウ]
+].freeze
+
 describe Kittenizer do
   it 'has a version number' do
     expect(Kittenizer::VERSION).not_to be_nil
   end
-
-  WORD_PAIRS = [
-    %w[こんばんは こんばんは],
-    ['こんにちは。', 'こんにちはにゃ。'],
-    ['あのとき食べたアレは本当においしかった。。。', 'あのとき食べたアレは本当においしかったにゃ。。。'],
-    %w[あれは一度食べておかないとー あれは一度食べておかにゃいとー],
-    %w[バナナ バニャニャ],
-    %w[おねがいします おねがいしみゃす],
-    %w[おはよう おはにゃう],
-    %w[オハヨウ オハニャウ]
-  ].freeze
 
   describe 'String#kittenize' do
     WORD_PAIRS.each do |word_pair|
@@ -28,14 +28,13 @@ describe Kittenizer do
 
   describe 'String#kittenize!' do
     WORD_PAIRS.each do |word_pair|
-      context "when @str = '#{word_pair[0]}', then @str.kittenize!" do
-        before do
-          @str = word_pair[0].dup
-          @str.kittenize!
-        end
+      context "when str = '#{word_pair[0]}', then str.kittenize!" do
+        let(:str) { word_pair[0].dup }
+
+        before { str.kittenize! }
 
         it "changes #{word_pair[1]}" do
-          expect(@str).to eq word_pair[1]
+          expect(str).to eq word_pair[1]
         end
       end
     end
